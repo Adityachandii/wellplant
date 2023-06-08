2339704f9e59dd6200d215ae947d6f7bfa508213
Adityachandii <76204985+Adityachandii@users.noreply.github.com>
2023-06-08 21:11:58 +0700

    first commit

---
+++ database/migrations/2023_06_08_114211_create_vouchers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVouchersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('jenis_voucher');
+            $table->string('image_voucher');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+}
